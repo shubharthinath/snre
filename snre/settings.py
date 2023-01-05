6bec73d65170ed78a3174ebb194a9d132770481c
Shubharthi <shubharthi24nath@gmail.com>
2023-01-05 20:10:18 +0530

    added static files

--- snre/settings.py
+++ snre/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
+    os.path.join(BASE_DIR, 'snre/static')
 ]
 
 
