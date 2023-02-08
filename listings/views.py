818be3aff37fff02f653d21f3e42195747be9c56
Shubharthi <shubharthi24nath@gmail.com>
2023-02-08 13:03:29 +0530

    displayed listings in template

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+#from .choices import price_choices, bedroom_choices, state_choices
+
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+  listings = Listing.objects.all()
 
-def listings(request):
-    return render(request, 'listings/listing.html')
+  paginator = Paginator(listings, 3)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+
+  context = {
+    'listings': paged_listings
+  }
+
+  return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+  listing = get_object_or_404(Listing, pk=listing_id)
+
+  context = {
+    'listing': listing
+  }
+
+  return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
