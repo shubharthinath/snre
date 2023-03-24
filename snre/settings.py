51024ea6e92aef56ce758b2d6836e046c8038463
Shubharthi <shubharthi24nath@gmail.com>
2023-03-25 04:24:48 +0530

    security check and email sending added

--- snre/settings.py
+++ snre/settings.py
@@ -140,5 +140,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "shubharthi24nath@gmail.com"
-EMAIL_HOST_PASSWORD = "lsgqgrdinjiwzdze"
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
